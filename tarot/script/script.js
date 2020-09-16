31bbbf71874aa4408358e3ad57cf3545e3ff61b0
Danielle Broadaway <danielle.broadaway@gmail.com>
2020-09-15 22:09:04 -0500

    Fleshed out wand card objects.  Changed " to ' on variable definitions.

--- tarot/script/script.js
+++ tarot/script/script.js
@@ -2,7 +2,7 @@ var deck = [];
 var newDeck = [];
 var cardImage = new Image();
 
-function randomNumber(max) {
+function randomMax(max) {
     var number = Math.random() * 100;
 
     while (number > max) {
@@ -25,7 +25,7 @@ function sortDeck() {
     Pentacles.forEach(addCard);
     Swords.forEach(addCard);
     Wands.forEach(addCard);
-    document.getElementById("card").innerHTML = '<img src="img/blank.jpg " /><figcaption>Draw A Card</figcaption>'
+    document.getElementById("card").innerHTML = "<img src= 'img/blank.jpg' /><figcaption>Draw A Card</figcaption>";
 }
 
 function drawCard() {
@@ -34,7 +34,7 @@ function drawCard() {
         deck.splice(0, 1);
         console.log(deck.length);
         console.log(card);
-        document.getElementById("card").innerHTML = '<img src="' + card.file + ' " /><figcaption>' + card.name + '</figcaption>'
+        document.getElementById("card").innerHTML = "<img src= ' " + card.file + " '/><figcaption>" + card.name + "</figcaption>";
     }
 }
 
@@ -44,16 +44,16 @@ function shuffleDeck() {
     var r;
     var newDeck = [];
     for (i = 0; i < 78; i++) {
-        r = randomNumber(deck.length);
+        r = randomMax(deck.length);
         newDeck.push(deck[r]);
         deck.splice(r, 1);
     }
     deck = newDeck;
     console.log(deck);
-    document.getElementById("card").innerHTML = '<img src="img/blank.jpg " /><figcaption>Draw A Card</figcaption>'
+    document.getElementById("card").innerHTML = "<img src='img/blank.jpg' /><figcaption>Draw A Card</figcaption>";
 }
 function notify(i) {
-    console.log("loaded " + deck[i].name)
+    console.log('loaded ' + deck[i].name)
 }
 function preloadDeck() {
     for (i = 0; i < deck.length; i++) {
--- tarot/script/swords.js
+++ tarot/script/swords.js
@@ -1,137 +1,137 @@
 var SwordsSuit = {
-    name : "Swords",
-    desc : "",
-    suit : "Spades",
-    element : "",
-    astrology : ""
+    name : 'Swords',
+    desc : '',
+    suit : 'Spades',
+    element : '',
+    astrology : ''
 };
 var AceSwords = {
-    name : "Ace Of Swords",
-    desc : "",
+    name : 'Ace Of Swords',
+    desc : '',
     number : 1,
-    file: "img/1_AceSwords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/1_AceSwords.jpg',
+    meaning : '',
+    reverse : ''
 };
 var TwoSwords = {
-    name : "Two of Swords",
-    desc : "",
+    name : 'Two of Swords',
+    desc : '',
     number : 2,
-    file: "img/2_Swords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/2_Swords.jpg',
+    meaning : '',
+    reverse : ''
 };
 var ThreeSwords = {
-    name : "Three Of Swords",
-    desc : "",
+    name : 'Three Of Swords',
+    desc : '',
     number : 3,
-    file: "img/3_Swords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/3_Swords.jpg',
+    meaning : '',
+    reverse : ''
 };
 var FourSwords = {
-    name : "Four Of Swords",
-    desc : "",
+    name : 'Four Of Swords',
+    desc : '',
     number : 4,
-    file: "img/4_Swords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/4_Swords.jpg',
+    meaning : '',
+    reverse : ''
 };
 var FiveSwords = {
-    name : "Five Of Swords",
-    desc : "",
+    name : 'Five Of Swords',
+    desc : '',
     number : 5,
-    file: "img/5_Swords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/5_Swords.jpg',
+    meaning : '',
+    reverse : ''
 };
 
 var SixSwords = {
-    name : "Six Of Swords",
-    desc : "",
+    name : 'Six Of Swords',
+    desc : '',
     number : 6,
-    file: "img/6_Swords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/6_Swords.jpg',
+    meaning : '',
+    reverse : ''
 };
 
 var SevenSwords = {
-    name : "Seven Of Swords",
-    desc : "",
+    name : 'Seven Of Swords',
+    desc : '',
     number : 7,
-    file: "img/7_Swords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/7_Swords.jpg',
+    meaning : '',
+    reverse : ''
 };
 
 var EightSwords = {
-    name : "Eight Of Swords",
-    desc : "",
+    name : 'Eight Of Swords',
+    desc : '',
     number : 8,
-    file: "img/8_Swords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/8_Swords.jpg',
+    meaning : '',
+    reverse : ''
 };
 var NineSwords = {
-    name : "Nine of Swords",
-    desc : "",
+    name : 'Nine of Swords',
+    desc : '',
     number : 9,
-    file: "img/9_Swords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/9_Swords.jpg',
+    meaning : '',
+    reverse : ''
 };
 
 var TenSwords = {
-    name : "Ten Of Swords",
-    desc : "",
+    name : 'Ten Of Swords',
+    desc : '',
     number : 10,
-    file: "img/10_Swords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/10_Swords.jpg',
+    meaning : '',
+    reverse : ''
 };
 
 var PageSwords = {
-    name : "Page Of Swords",
-    desc : "",
+    name : 'Page Of Swords',
+    desc : '',
     number : 11,
-    file: "img/11_PageSwords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/11_PageSwords.jpg',
+    meaning : '',
+    reverse : ''
 };
 
 var KnightSwords = {
-    name : "Knight Of Swords",
-    desc : "",
+    name : 'Knight Of Swords',
+    desc : '',
     number : 12,
-    file: "img/12_KnightSwords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/12_KnightSwords.jpg',
+    meaning : '',
+    reverse : ''
 };
 
 var QueenSwords = {
-    name : "Queen of Swords",
-    desc : "",
+    name : 'Queen of Swords',
+    desc : '',
     number : 13,
-    file: "img/13_QueenSwords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/13_QueenSwords.jpg',
+    meaning : '',
+    reverse : ''
 };
 
 var KingSwords = {
-    name : "King of Swords",
-    desc : "",
+    name : 'King of Swords',
+    desc : '',
     number : 14,
-    file: "img/14_KingSwords.jpg",
-    meaning : "",
-    reverse : ""
+    file: 'img/14_KingSwords.jpg',
+    meaning : '',
+    reverse : ''
 };
 
 /*var MinorCard = {
-    name : "",
-    desc : "",
+    name : '',
+    desc : '',
     number : 0,
-    file:"",
-    meaning : "",
-    reverse : ""
+    file:'',
+    meaning : '',
+    reverse : ''
 };*/
 var Swords = [AceSwords, TwoSwords, ThreeSwords, FourSwords, FiveSwords, SixSwords, SevenSwords, EightSwords, NineSwords, TenSwords, PageSwords, KnightSwords, QueenSwords, KingSwords];
